0ee14440486589267907e61a8eed01466148c9ad
Luis Herrera <lui-andre@hotmail.com>
2020-11-07 00:38:59 -0500

    Deploy puerto 443 peerjs

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 const peerServer = PeerServer({
-    port: 3001,
+    port: 443,
     ssl: {
         key: fs.readFileSync('./ssl/privkey.pem'),
         cert: fs.readFileSync('./ssl/fullchain.pem')
