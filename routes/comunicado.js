e12bf0b31bff2fc60d14fb7fa22dfd481727e60f
Luis Herrera <lui-andre@hotmail.com>
2020-11-27 01:30:56 -0500

    Deploy solucionado comunicado

--- routes/comunicado.js
+++ routes/comunicado.js
@@ -13,7 +13,7 @@ const router = Router();
 
 router.get('/', validarJWT, getComunicados);
 
-router.get('/:id', validarJWT, getComunicados);
+router.get('/:id', validarJWT, getComunicado);
 
 router.post('/', [
         validarJWT,
